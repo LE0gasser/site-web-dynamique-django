4e6475b78196be2707bc929eb47c58e20edc64fd
e2500616 <leo.gasser682006@gmail.com>
2026-05-06 16:47:53 +0200

    push

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'bibliotheque.apps.BibliothequeConfig',
 ]
 
 ROOT_URLCONF = 'firstproject.urls'
